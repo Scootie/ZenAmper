a5858dd7e517c0c6fef23b61ca41dc856814f3be
Caleb Ku <Scootie@users.noreply.github.com>
2014-12-05 15:30:14 -0600

    Create zenamp_settings.py

---
+++ zenamp_settings.py
@@ -0,0 +1,4 @@
+ZENMINER_USERNAME = 'myusername'
+ZENMINER_PASSWORD = 'mypassword'
+PREFERENCE = 'Boost' #Options are 'DD' and 'Boost'
+DD_ORDER = 'HashPoint Pool' #Other Options: 'ZenPool','CleverHashlet','HashletBit', 'LTC Pool', 'WaffleHashlet','MultiHashlet'
